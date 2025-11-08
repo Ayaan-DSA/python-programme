e4e45957873660e2ab077e9c32a621ebda42cdcc
Ayaan-DSA <ayaankhanak3589@gmail.com>
2025-11-08 11:55:52 +0530

    class

--- cls.py
+++ cls.py
@@ -4,11 +4,24 @@
 # class my_car():
 #     pass
 
-class Car:
-    def __init__(self , brand , model):
-        self.brand = brand 
-        self.model = model
+# class Car:
+#     def __init__(self , brand , model):
+#         self.brand = brand 
+#         self.model = model
 
-car1 = Car("BMW" , "XS")
-print(car1.brand) #BMW
-print(car2.model) #XS
+# car1 = Car("BMW" , "XS")
+# print(car1.brand) #BMW
+# print(car2.model) #XS
+
+class college :
+    colleges = " abc college"
+
+    def __init__(self, name):
+        self.name = name
+s1 = college("abhi")
+s2 = college ("adi")
+
+print(s1.name) #abhi
+print(s2.name) #adi
+print(college.colleges) #abc college
+        
